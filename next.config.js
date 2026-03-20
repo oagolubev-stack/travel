57a9550a9f41b8d57cf74109aeca27316abcf7e1
oagolubev-stack <o.a.golubev@gmail.com>
2026-03-20 18:46:17 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
-    domains: ["images.unsplash.com", "via.placeholder.com"], // добавьте свои домены
+    domains: ["images.unsplash.com", "via.placeholder.com"],
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
