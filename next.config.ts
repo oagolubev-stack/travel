483a1c61ad1bde962dd51c4a848b50e4bc0d1899
oagolubev-stack <o.a.golubev@gmail.com>
2026-03-20 18:36:43 +0300

    Delete next.config.ts

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  images: {
-    domains: ["images.unsplash.com", "via.placeholder.com"], // добавьте свои домены
-  },
-};
-
-export default nextConfig;
